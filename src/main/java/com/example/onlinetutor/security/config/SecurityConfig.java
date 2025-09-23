7065b4c2b2fb1e1daa3d2603724290971e4ecfe4
Radka Nhapulo <radka.fnhapulo@gmail.com>
2025-09-23 22:59:22 +0300

    used 'SecurityFilterChain' instead of WebSecurityConfigurerAdapter

--- src/main/java/com/example/onlinetutor/security/config/SecurityConfig.java
+++ src/main/java/com/example/onlinetutor/security/config/SecurityConfig.java
@@ -2,21 +2,66 @@ package com.example.onlinetutor.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration {
+@Configuration
+public class SecurityConfig {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
 
     @Autowired
-    @Qualifier("customUserDetailsService")
     private UserDetailsService userDetailsService;
+
+
+    public SecurityConfig (PasswordEncoder passwordEncoder,
+                           UserDetailsService userDetailsService) {
+        this.passwordEncoder = passwordEncoder;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/signUp").permitAll()
+        );
+
+        http.formLogin(form -> form
+                .loginPage("/login")
+                .usernameParameter("username")
+                .defaultSuccessUrl("/profile", true)
+                .failureUrl("/signIn?error")
+                .permitAll()
+        );
+
+        return http.build();
+    }
 
 
 }
