4735d2362383e8f03bd40432928ef110653b6afb
Radka Nhapulo <radka.fnhapulo@gmail.com>
2025-10-01 21:14:19 +0300

    not able to see the onboarding and also not redirecting to choose subjects

--- src/main/java/com/example/onlinetutor/repositories/AptitudeTestRepo.java
+++ src/main/java/com/example/onlinetutor/repositories/AptitudeTestRepo.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Optional;
+
 @Repository
 public interface AptitudeTestRepo extends JpaRepository<AptitudeTest,Long> {
     Optional<AptitudeTest> findByUserId(Long userId);
