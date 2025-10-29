0ec57224bea22bc4b1b07d7e63a676a9f00dac36
Radka Nhapulo <radka.fnhapulo@gmail.com>
2025-10-29 14:20:34 +0300

    issue with analysis

--- src/main/java/com/example/onlinetutor/dto/TutorAnalyticsDTO.java
+++ src/main/java/com/example/onlinetutor/dto/TutorAnalyticsDTO.java
@@ -1,13 +1,19 @@
 package com.example.onlinetutor.dto;
 
-import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TutorAnalyticsDTO {
+
     private String articleTitle;
     private long studentsPassed;
     private long studentsFailed;
 
-    public TutorAnalyticsDTO(String articleTitle, long passed, long failed) {
-    }
-//    private Map<String, Long> commonMistakes;
+//    TODO: The commonly failed questions field must be here
 }
